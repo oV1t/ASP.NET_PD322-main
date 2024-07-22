7f7bbad9acf356c6135d33aed8d1249a73d1bc9b
oV1ce e <omax51851@gmail.com>
2024-07-22 14:47:47 +0300

    Initial commit

---
+++ WebHalk/Controllers/MainController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebHalk.Data;
+using WebHalk.Data.Entities;
+using WebHalk.Models.Categories;
+
+namespace WebHalk.Controllers
+{
+    public class MainController : Controller
+    {
+        private readonly HulkDbContext _hulkDbContext;
+
+        public MainController(HulkDbContext hulkDbContext)
+        {
+            _hulkDbContext = hulkDbContext;
+        }
+        public IActionResult Index()
+        {
+            var list = _hulkDbContext.Categories
+                .Select(x=> new CategoryItemViewModel
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                    Image = x.Image
+                })
+                .ToList();
+            return View(list);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(CategoryCreateViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            CategoryEntity entity = new CategoryEntity()
+            {
+                Image = model.Image,
+                Name = model.Name,
+            };
+            _hulkDbContext.Categories.Add(entity);
+            _hulkDbContext.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ WebHalk/Models/Categories/CategoryCreateViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebHalk.Models.Categories
+{
+    public class CategoryCreateViewModel
+    {
+        [Display(Name="Назва")]
+        [Required(ErrorMessage ="Вкажіть назву категорії")]
+        public string Name { get; set; } = string.Empty;
+        [Display(Name = "Фото url")]
+        [Required(ErrorMessage = "Вкажіть адерсу фото")]
+        public string Image { get; set; } = string.Empty;
+    }
+}
